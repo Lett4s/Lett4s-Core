ef4fcefb5491908834d27c41a9c1beed99dd5800
lett4s <twestenfeld@gmx.de>
2026-02-05 20:09:29 +0100

    Small LoginUI additions  - LHintTextField limit input test  - Register added to LoginUI

--- UI/src/main/java/util/StaticConstant.java
+++ UI/src/main/java/util/StaticConstant.java
@@ -4,16 +4,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class StaticConstant {
-    public static final String NAME = "CLIENT";
-    public static final String PROJECT = "Client-Project";
+
+    // DYNAMIC CONSTANTS
+    public static String PROJECT = "Client-Project";
+    public static String USER_AGENT = "Client";
+    public static String NAME = "CLIENT";
+    public static String VERSION;
 
     // PREDEFINED CONSTANTS
+    public static final String PLAYER_SETTING_fILE = ".user-settings";
     public static final String PROJECT_DATA = "project.json";
     public static final Path APPLICATION_SETTINGS = Paths.get(System.getProperty("user.home")).resolve(PROJECT);
     public static final String CLIENT_SETTING_fILE = ".client-settings";
-    public static final String PLAYER_SETTING_fILE = ".user-settings";
-    public static final String USER_AGENT = "Client";
-    // DYNAMIC CONSTANTS
-    public static String VERSION;
+
 
 }
